844425275937fdc6bec2ac030a57ae161daf805f
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-16 21:01:26 +0800

    MainButton is now generic

--- src/components/MainButton.js
+++ src/components/MainButton.js
@@ -1,67 +1,28 @@
-import React, { useState } from 'react';
-import { StyleSheet, TouchableHighlight, View } from "react-native";
+import React from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 
-import SemiBoldText from './SemiBoldText';
 import Colors from '../constants/colors';
 import Dimens from '../constants/dimens';
 
-export default function MainButton(props) {
-    const [pressed, setPressed] = useState(false);
+import Button from "./Button";
 
+export default function MainButton(props) {
     return (
-        <TouchableHighlight
-            style={{
-                ...(pressed ? styles.buttonPressed : styles.buttonNotPressed),
-                ...(pressed ? Dimens.mainButtonBoxShadowPressed : Dimens.mainButtonBoxShadow),
-                ...props.style
-            }}
-            activeOpacity={1}
-            underlayColor={"rgba(0,0,0,0)"}
-            onShowUnderlay={() => setPressed(true)}
-            onHideUnderlay={() => setPressed(false)}
-            onPress={props.onPress}
-        >
-            <LinearGradient
-                start={{x: 0, y: 0}}
-                end={{x: 1, y: 1}}
-                colors={[Colors.pink, Colors.reddishPurple]}
-                style={{
-                    ...styles.button,
-                    ...(pressed ? Dimens.mainButtonBoxShadowPressed : Dimens.mainButtonBoxShadow)
-                }}
-            >
-                {props.imageLeft ? <View style={{...styles.image, marginRight: Dimens.mainButtonImageMargin}}>{props.imageLeft}</View> : null}
-                <SemiBoldText color={Colors.white} size={Dimens.mainButtonTextSize}>{props.caption}</SemiBoldText>
-                {props.imageRight ? <View style={{...styles.image, marginLeft: Dimens.mainButtonImageMargin}}>{props.imageRight}</View> : null}
-            </LinearGradient>
-        </TouchableHighlight>
+        <Button
+            shadow={true}
+            textColor={Colors.white}
+            textSize={Dimens.mainButtonTextSize}
+            caption={props.caption}
+            onPress={() => {}}
+            imageLeft={props.imageLeft}
+            imageRight={props.imageRight}
+            renderContainer={() => (
+                <LinearGradient
+                    start={{x: 0, y: 0}}
+                    end={{x: 1, y: 1}}
+                    colors={[Colors.pink, Colors.reddishPurple]}
+                />
+            )}
+        />
     );
 }
-
-const styles = StyleSheet.create({
-    button: {
-        flexDirection: "row",
-        paddingVertical: Dimens.mainButtonPaddingVertical,
-        paddingHorizontal: Dimens.mainButtonPaddingHorizontal,
-        alignItems: "center",
-        justifyContent: "center",
-        height: Dimens.mainButtonHeight,
-        borderRadius: Dimens.mainButtonHeight / 2
-    },
-
-    buttonNotPressed: {
-        top: 0
-    },
-
-    buttonPressed: {
-        top: Dimens.mainButtonPressedNudgeDistance
-    },
-    
-    image: {
-        width: Dimens.mainButtonImageSize,
-        height: Dimens.mainButtonImageSize,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-});
