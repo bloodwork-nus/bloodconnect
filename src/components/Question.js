0e90c879a0361e7e8835b7269b35d64923f09b2d
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-17 20:58:10 +0800

    Question now supports info

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
+import { TouchableRipple } from "react-native-paper";
 
 import Colors from '../constants/colors';
 import Dimens from '../constants/dimens';
@@ -9,12 +11,26 @@ import FontText from "../components/FontText";
 export default (props) => {
     const {
         prompt,
-        content
+        content,
+        info
     } = props;
+
+    const showInfo = () => {
+        alert(info);
+    }
 
     return (
         <View style={{...styles.question, ...props.style}}>
-            <FontText flavor="medium" size={17} color={Colors.darkBlue} style={{marginBottom: 10}}>{prompt}</FontText>
+            <View style={styles.prompt}>
+                <FontText flavor="medium" size={17} color={Colors.darkBlue}>{prompt}</FontText>
+
+                {info ? 
+                    <TouchableRipple onPress={showInfo} borderless={true} style={styles.touchable}>
+                        <MaterialIcon name="info" size={23} color={Colors.grey2} />
+                    </TouchableRipple>
+                : null}
+            </View>
+
             <View style={styles.content}>
                 {content()}
             </View>
@@ -24,5 +40,21 @@ export default (props) => {
 const styles = StyleSheet.create({
     question: {
         width: "100%"
+    },
+
+    prompt: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 10,
+        justifyContent: "space-between"
+    },
+
+    touchable: {
+        width: 25,
+        height: 25,
+        borderRadius: 100,
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: 10
     }
 });
