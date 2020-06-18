6c954c6481aceb1d8cbd96ff8d1ef34c9a88c7d1
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-18 20:43:42 +0800

    Added BottomSheetModal

---
+++ src/components/BottomSheetModal.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
+import BottomSheet from "reanimated-bottom-sheet";
+import Animated from "react-native-reanimated";
+
+import Colors from "../constants/colors";
+
+export default (props) => {
+    const {
+        height,
+        visible,
+        onClose,
+        renderContent
+    } = props;
+
+    let modalRef;
+    let fade = new Animated.Value(0);
+
+    useEffect(() => {
+        modalRef.snapTo(visible ? 0 : 1);
+    });
+
+    return (<>
+        {visible ? 
+            <TouchableWithoutFeedback onPress={() => modalRef.snapTo(1)}>
+                <Animated.View style={{
+                    width: "100%",
+                    height: "100%",
+                    opacity: Animated.sub(0.7, fade),
+                    backgroundColor: Colors.black,
+                    position: "absolute"
+                }} />
+            </TouchableWithoutFeedback>
+        : null}
+
+        <BottomSheet
+            ref={ref => modalRef = ref}
+            initialSnap={1}
+            snapPoints={[height, 0]}
+            renderHeader={() => <View />}
+            renderContent={renderContent}
+            borderRadius={30}
+            callbackNode={fade}
+            onCloseEnd={onClose}
+        />
+    </>);
+}
