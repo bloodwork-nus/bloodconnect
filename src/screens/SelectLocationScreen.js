af51c1d3daf3c14f84161ea4e72ccc9507edb8a3
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-22 02:57:51 +0800

    Added empty SelectLocationScreen

---
+++ src/screens/SelectLocationScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import GenericSubScreen from "./GenericSubScreen";
+
+export default (props) => {
+    return (
+        <GenericSubScreen>
+            
+        </GenericSubScreen>
+    );
+}
+const styles = StyleSheet.create({
+    
+});
