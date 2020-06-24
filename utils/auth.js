a8347afdb4594cd5d3650a4f33edc424686e0c0a
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-24 20:44:24 +0800

    Added getCurrentUser functions

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,7 @@
 import firebase from "./firebase";
 
 export const signOut = () => firebase.auth().signOut();
+
+export const getCurrentUserUid = () => firebase.auth().currentUser ? firebase.auth().currentUser.uid : null;
+export const getCurrentUserName = () => firebase.auth().currentUser ? firebase.auth().currentUser.displayName : null;
+export const getCurrentUser = () => firebase.auth().currentUser;
