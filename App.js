3136260ec970ffab795e1b96f7ab776654cf29af
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-24 20:55:59 +0800

    Added new screens navigations

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as Font from "expo-font";
@@ -9,7 +9,13 @@ import IntroScreen from './src/screens/IntroScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import CreateAccountScreen from './src/screens/CreateAccountScreen';
 import ExploreScreen from './src/screens/ExploreScreen';
+import NewRequestFormScreen from "./src/screens/NewRequestFormScreen";
+import DonateScreen from './src/screens/DonateScreen';
+import UserProfileScreen from './src/screens/UserProfileScreen';
+import CompletedFormScreen from './src/screens/CompletedFormScreen';
+
 import firebase from "./utils/firebase";
+import requests from "./utils/requests";
 
 const fetchFonts = () => {
     return Font.loadAsync({
@@ -37,6 +43,10 @@ export default function App() {
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
                 <Stack.Screen name="Explore" component={ExploreScreen} />
+                <Stack.Screen name="NewRequestForm" component={NewRequestFormScreen} />
+                <Stack.Screen name="Donate" component={DonateScreen} />
+                <Stack.Screen name="UserProfile" component={UserProfileScreen} />
+                <Stack.Screen name="CompletedForm" component={CompletedFormScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
