2bb1478bfe54c2069e5897384d3be3160999804f
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-24 17:16:42 +0800

    GenericSubScrollableScreen supports top and bottom static components

--- src/screens/GenericSubScrollableScreen.js
+++ src/screens/GenericSubScrollableScreen.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { StyleSheet, ScrollView, KeyboardAvoidingView } from "react-native";
 
+import Colors from "../constants/colors";
+import Dimens from "../constants/dimens";
+
 import GenericSubScreen from "./GenericSubScreen";
 
 export default (props) => {
     const {
-        isKeyboardAvoiding
+        isKeyboardAvoiding,
+        renderOutsideScrollViewTop,
+        renderOutsideScrollViewBottom
     } = props;
 
     return (
         <GenericSubScreen>
+            {renderOutsideScrollViewTop ? renderOutsideScrollViewTop() : null}
+
             <KeyboardAvoidingView behaviour="height" enabled={isKeyboardAvoiding === false ? false : true}>
                 <ScrollView contentContainerStyle={styles.content}>
                     {props.children}
                 </ScrollView>
             </KeyboardAvoidingView>
+
+            {renderOutsideScrollViewBottom ? renderOutsideScrollViewBottom() : null}
         </GenericSubScreen>
     );
 }
