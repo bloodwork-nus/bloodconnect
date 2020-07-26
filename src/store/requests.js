153c70dc7b8134348ed9efa1b3797d5558685f8b
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-07-26 17:19:09 +0800

    Added requests store functions

--- src/store/requests.js
+++ src/store/requests.js
@@ -1,12 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
 
 const slice = createSlice({
     name: "requests",
     initialState: {
-
+        requests: { }
     },
     reducers: {
-
+        requestsUpdated: (state, action) => {
+            state["requests"] = action.payload.requests;
+        }
     }
 });
 
+const {
+    requestsUpdated
+} = slice.actions;
+
+export default slice.reducer;
+
+export const updateRequests = (requests) => requestsUpdated({ requests });
+
+export const getRequests = createSelector(
+    state => state.requests,
+    requests => requests.requests
+);
+
+export const getMyRequests = (requester) => createSelector(
+    state => state.requests,
+    requests => Object.values(requests.requests).filter((request) => request.requester === requester)
+);
